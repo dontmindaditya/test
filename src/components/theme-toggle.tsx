14154ccffe29d2ca488b5535d013197fabe553cb
Aditya Pamar <aditsingh8077@gmail.com>
2026-04-17 14:56:50 -0700

    Update theme-toggle.tsx

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -8,7 +8,7 @@ export default function ThemeToggle() {
   const { theme, setTheme } = useTheme();
   const [mounted, setMounted] = useState(false);
 
-  useEffect(() => setMounted(true), []);
+ 
 
   if (!mounted) return null;
 
